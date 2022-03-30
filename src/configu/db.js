eeb8723a05c52d3bde6f43a33097f0e141f9a455
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-03-30 22:57:21 +0530

    updated file

--- src/configu/db.js
+++ src/configu/db.js
@@ -1,5 +1,5 @@
-const mongoose=require("monogoose");
+const mongoose=require("mongoose");
 const connect =()=>{
-    return mongoose.connect("");
+    return mongoose.connect("mongodb+srv://Ritesh6997:Ritesh6997@cluster0.9cfqh.mongodb.net/ONNBike?retryWrites=true&w=majority");
 };
 module.exports=connect;
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const authenticate = require("../middleware/authenticate");
-const Product = require("../model/product.model");
+const Product = require("../model/productModel");
 
-router.post("", authenticate, async (req, res) => {
-
-    req.body.user_id = req.userID;
+router.post("", async (req, res) => {
     try{
         const product = await Product.create(req.body);
         return res.status(200).send(product);
@@ -18,32 +15,12 @@ router.post("", authenticate, async (req, res) => {
 
 router.get("", async (req, res) => {
     try{
-        const product = await Product.find()
+        const product = await Product.find().lean().exec();
         return res.status(200).send(product)
     }
     catch(err){
         return res.status(400).send({message : err.message})
     }
 });
-
-router.patch("/:id",authenticate,async(req,res)=>{
-    try {
-        const product = await Product.findByIdAndUpdate(req.params.id,req.body).lean().exec();
-        return res.status(200).send(product)
-    } catch (error) {
-        return res.status(400).send({message : err.message})
-        
-    }
-});
-
-router.delete('/:id',authenticate, async (req, res) => {
-    try {
-      const product = await Product.findByIdAndDelete(req.params.id);
-      return res.status(200).send(section)
-    } catch (err) {
-      return res.status(500).send({ message: err.message })
-    }
-  });
-
 
 module.exports = router;
