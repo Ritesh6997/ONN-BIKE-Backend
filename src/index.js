860c62420f79073516da59da5d2ecd2cc2b745f6
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-03-30 14:53:35 +0530

    Revert "Update"

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 const express=require("express");
 const app=express();
-// const Usercontroller=require("./controllers/usercontroller")
-const productController = require("./controllers/product.controller")
-const {Signup,login} = require("./controllers/auth.controller")
-const { body, validationResult } = require('express-validator')
-const SignupController=require("./controllers/SignupController")
-
 app.use(express.json());
-// app.post("/user",Usercontroller);
-app.use("/users", userController);
-app.use("/Signup", SignupController);
-app.post("/login",validator,loginController);
-app.use("/products", productController);
+
 
 module.exports=app;
--- src/model/AreaModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-
-const areaSchema=new mongoose.Schema({
-    stop:{type:String,required:true},
-    stop_landmark:{type:String,required:true},
-    cityId:{type:mongoose.Schema.Types.ObjectId,
-        ref:"city",
-        required:true}
-});
