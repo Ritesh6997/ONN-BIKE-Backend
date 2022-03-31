0193df0128413b068bb579493f504d2155ed0146
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-03-31 12:47:24 +0530

    modified files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express=require("express");
 const app=express();
-// const Usercontroller=require("./controllers/usercontroller")
-const productController = require("./controllers/product.controller")
+const Usercontroller=require("./controllers/userController")
+const productController = require("./controllers/productController")
 const SignupController=require("./controllers/SignupController")
 const loginController=require("./controllers/loginController")
 
 app.use(express.json());
-// app.post("/user",Usercontroller);
-// app.use("/users", userController);
+app.use("/users", Usercontroller);
 app.use("/Signup", SignupController);
 app.use("/login",loginController);
 app.use("/products", productController);
